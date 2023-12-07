58fdb1194298fbec60c05557d238037bc864bc50
Soumt <rltks1305@naver.com>
2023-12-07 22:03:53 +0900

    feat: login page functions

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
-from fastapi import Depends, FastAPI, Request
+from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_users.router import oauth as oauth_router
+from fastapi_users.authentication import JWTStrategy
+
+from users import get_user_manager, UserManager
 from typing import Dict
 
 from app.db import User, create_db_and_tables
@@ -16,26 +20,68 @@ from app.users import (
 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+import random
 
 app = FastAPI()
+
+#session middleware
+app.add_middleware(SessionMiddleware, secret_key=SECRET)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 current_user_optional = fastapi_users.current_user(optional=True)
 
-
 @app.get("/")
 async def root(request: Request, user: User = Depends(current_user_optional)):
     return templates.TemplateResponse("home.html", {"request": request, "user": user})
 
-
 @app.get("/account/login")
 async def login(request: Request, user: User = Depends(current_user_optional)):
-    if user:
-        return RedirectResponse("/")
-    return templates.TemplateResponse("auth/login.html", {"request": request})
+    redirect_uri = request.query_params.get("next")
+    failed = request.session.pop("failed")
 
+    #random string
+    csrf_token = "".join([random.choice("0123456789abcdef") for _ in range(32)])
+    request.session["csrf_token"] = csrf_token
+
+
+    if redirect_uri:
+        if not redirect_uri.startswith("/"):
+            redirect_uri = None
+        request.session["next"] = redirect_uri
+    if user:
+        return RedirectResponse(redirect_uri or "/")
+    
+    print(request.session.get("next"))
+    return templates.TemplateResponse("auth/login.html", {"request": request, "csrf_token": csrf_token , "failed": failed})
+
+@app.post("/account/login")
+async def login(request: Request, credentials: OAuth2PasswordRequestForm = Depends(), user_manager: UserManager = Depends(get_user_manager), strategy: JWTStrategy = Depends(auth_backend.get_strategy) ):
+    # request form
+    form = await request.form()
+    # check csrf token
+    if form.get("csrf_token") != request.session.get("csrf_token"):
+        request.session["failed"] = 2
+        return RedirectResponse("/account/login")
+    
+    
+    next_url = request.session.get("next")
+    if next_url:
+        request.session.pop("next")
+        response.headers["location"] = next_url
+        response.status_code = 302
+
+    user = await user_manager.authenticate(credentials)
+
+    if user is None or not user.is_active:
+        request.session["failed"] = 1
+        return RedirectResponse("/account/login")
+    
+    response = await auth_backend.login(strategy, user)
+    await user_manager.on_after_login(user, request, response)
+    return response
 
 @app.get("/account/register")
 async def register(request: Request, user: User = Depends(current_user_optional)):
@@ -65,7 +111,7 @@ async def oauth_login(request: Request, provider: str):
 
 
 app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth", tags=["auth"]
+    fastapi_users.get_auth_router(auth_backend), prefix="/account", tags=["auth"]
 )
 
 app.include_router(
